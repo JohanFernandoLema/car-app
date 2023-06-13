d4d7c34ef84e8c3331a538ffa25b7ffae368feef
JohanFernandoLema <johan.13fernando@gmail.com>
2023-06-13 17:28:54 -0400

    create searchmanufacturer component inside searchbar component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,4 +1,18 @@
+'use-client'
+import SearchManufacturer from './SearchManufacturer'
+import { useState } from 'react'
 const SearchBar = () => {
-  return <div>SearchBar</div>
+  const [manufacturer, setManufacturer] = useState('')
+  const handleSearch = () => {}
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
+    </form>
+  )
 }
 export default SearchBar
